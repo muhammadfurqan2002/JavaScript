af62d48c7f7d807e4aaa8f00a092f54f14c1e9fd
Muhammad Furqan <furqan.b2002@gmail.com>
2025-08-14 01:43:35 +0500

    DoM completed

--- phase9.js
+++ phase9.js
@@ -86,4 +86,84 @@ span.classList.toggle("hlo")
 
 
 
+// what is dom? how does it represents the html structre;
+// it's tree like strutcure and everything thing wirtten inside html is node
 
+
+// Name the types of nodes in the Dom? 
+// Node, Element, Text, Comment
+
+
+// what's the difference between and element node and text node?
+// html element is element node and inside it text called text node . it's alos has child element
+
+
+
+//  what is difference between getElementById and querySelector?
+//  getElementById we get only id and while queryselector is universal
+
+
+// what does getElementById return ?
+//  html collection  simply you can consider like array
+
+
+
+
+
+
+// child nodes
+
+let main=document.getElementsByClassName("main")
+
+console.log(typeof main[0].childNodes);
+
+console.log(main[0].firstChild)
+console.log(main[0].lastChild)
+console.log(main[0].firstElementChild)
+main[0].lastElementChild.textContent="i hope you are well"
+console.log(main[0].lastElementChild.innerText)
+console.log(typeof main[0].lastElementChild.innerHTML )
+
+console.log(main[0].childNodes[1]===main[0].firstChild)
+console.log(main[0].childNodes[1].hasChildNodes)
+
+
+
+// Matches , closest, contains  // these only works on elements or variables
+
+
+// for checking element wheather it's speified or not
+console.log(main[0].matches("p"))
+
+
+// closet used to find closest ancestor start form self and then goes up in search of ancestor
+
+let p=document.getElementsByClassName("child2")
+let main_div=p[0].closest(".main")
+console.log(p[0])
+console.log(main_div)
+
+
+
+// contains check whether element exist or not inside parent
+
+console.log(main[0].contains(p[0]))
+console.log(p[0].contains(main[0]))
+
+
+
+
+
+// InsertAdjacent HTML/Text/Element
+
+let sample=document.createElement("div")
+
+sample.innerText=" hljfsd lkjdslkfs lkjdlkgd lkjdklfsjs lkjglkdfg ";
+
+
+
+let ad=document.querySelector("#dom")
+
+// ad.insertAdjacentElement("beforeend",sample)
+// ad.insertAdjacentElement("afterbegin",sample)
+// ad.insertAdjacentElement("beforebegin",sample)
